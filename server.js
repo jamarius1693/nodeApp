e804fdbb836ce34e90110d74b96934c0297bf506
marius <m.i.petrovan@rgu.ac.uk>
2015-11-23 14:30:04 +0000

    more text

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 
 var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead(300, {"Content-Type": "text/html"});
     response.write("<!DOCTYPE 'html'>");
     response.write("<html>");
     response.write("<head>");
